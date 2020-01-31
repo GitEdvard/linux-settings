21bbfaa9bc7aaf8ab97a6e4da7411c14aaf8249e
Edvard Englund <edvard.englund@medsci.uu.se>
2020-01-31 15:28:44 +0100

    Fix script delete-bransh.sh

--- bin/delete-branch.sh
+++ bin/delete-branch.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 # Usage: <repository name> 
 if [[ $# -ne 2 ]]; then
-	echo "Usage:  git-merge-develop.sh <repository name>
+	echo "Usage:  delete-branch.sh <repository name> <branch name>"
 	exit 1	
 fi
-git fetch "$1" develop
-git merge "$1"/develop
+git br -D "$2"
+git push --delete "$1" "$2"
 
