c78e4cb87adee44bdd2e2d4c1031afeda5e34e98
Edvard Englund <edvard.englund@medsci.uu.se>
2020-03-25 10:03:48 +0100

    Make initial validation in checkin/checkout script

--- bin_home/checkin_documents.sh
+++ bin_home/checkin_documents.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+set -e
+
+if [ "$SNPSEQ_WORKSTATION_USERNAME" == "" ];  then
+	echo "Error: SNPSEQ_WORKSTATION_USERNAME must be set"
+	exit 1;
+fi
+
+
+if [ "$SNPSEQ_WORKSTATION_HOSTNAME" == "" ];  then
+	echo "Error: SNPSEQ_WORKSTATION_HOSTNAME must be set"
+	exit 1;
+fi
+
 scp -rp $HOME/Documents $SNPSEQ_WORKSTATION_USERNAME@$SNPSEQ_WORKSTATION_HOSTNAME:$HOME
