2feeb6d616fe859320b7863cd8f04ae4201fe2e8
Edvard Englund <edvard.englund@medsci.uu.se>
2020-10-15 09:29:22 +0200

    Delete bin directories, they are moved to bashrc-contents

--- bin_work/commit.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-# Usage: <commit message> 
-if [[ $# -ne 1 ]]; then
-	echo "Usage:  commit <message>"
-	exit 1	
-fi
-git add .
-git ci -m "$1"
-
--- bin_work/grep-commonlims.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-# Usage: <search phrase> 
-if [[ $# -ne 1 ]]; then
-	echo "Usage:  grep-commonlims <search phrase>"
-	exit 1	
-fi
-grep -r "$1" --exclude-dir=node_modules
